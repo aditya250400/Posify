19e2f1cbfab590202f174f120c85be342f55a230
Muhamad Rizki Aditya <muhamadrizkiaditya32@gmail.com>
2026-05-02 00:03:15 +0700

    add jwt for authentication

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  const token = req.headers["authorization"];
+
+  if (!token) return res.status(401).json({ message: "Tidak terautentikasi" });
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) return res.status(401).json({ message: "Token tidak valid" });
+
+    next();
+  });
+};
+
+module.exports = verifyToken;
