7756bda604b8da55e0a9408b8654ffb4aeb37a9d
Muhamad Rizki Aditya <muhamadrizkiaditya32@gmail.com>
2026-05-07 12:47:35 +0700

    add axios setup

---
+++ frontend/src/services/api.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+
+const Api = axios.create({
+  baseURL: import.meta.env.VITE_APP_BASEURL,
+});
+
+Api.interceptors.response.use(
+  function (response) {
+    const token = Cookies.get("token");
+
+    if (token) {
+      response.headers.Authorization = JSON.parse(token);
+    }
+
+    return response;
+  },
+  (error) => {
+    if (401 === error.response.status) {
+      Cookies.remove("token");
+      window.location = "/";
+    } else {
+      return Promise.reject(error);
+    }
+  },
+);
