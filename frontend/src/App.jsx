d4a8b8bde9845f4048f4d7bd0807cce1e1a800fb
Muhamad Rizki Aditya <muhamadrizkiaditya32@gmail.com>
2026-05-07 20:51:21 +0700

    add theme states

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
 import { Toaster } from "react-hot-toast";
+import { useStore } from "./states/theme";
+import { useEffect } from "react";
 
 function App() {
+  const { theme } = useStore();
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-bs-theme", theme);
+  }, [theme]);
   return (
     <>
       <Toaster />
