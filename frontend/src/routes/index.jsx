f099cd076b4cdf80558c65a8f841657a5bb87ddb
Muhamad Rizki Aditya <muhamadrizkiaditya32@gmail.com>
2026-05-08 09:15:53 +0700

    add admin layout

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useStore } from "../states/user";
 import Login from "../views/auth/login";
+import Dashboard from "../views/dashboard";
 
 export default function AppRoutes() {
   const { token } = useStore();
@@ -10,6 +11,10 @@ export default function AppRoutes() {
       <Route
         path="/"
         element={token ? <Navigate to="/dashboard" replace /> : <Login />}
+      />
+      <Route
+        path="/dashboard"
+        element={token ? <Dashboard /> : <Navigate to="/" replace />}
       />
     </Routes>
   );
